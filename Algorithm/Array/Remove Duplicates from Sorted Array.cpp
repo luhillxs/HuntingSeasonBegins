d70d159cd61d7a1180693e2b971d5bad3bdb4634
Hill Lo <hilllocanton@gmail.com>
2016-01-18 15:37:17 -0500

    submit codes

---
+++ Algorithm/Array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,24 @@
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+Do not allocate extra space for another array, you must do this in place with constant memory.
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=1;
+        int j=1;
+        if(nums.size()==0){
+            return 0;
+        }
+        else{
+            for(i=0;i<nums.size();i++){
+                if(nums[i]!=nums[j-1]){
+                    nums[j] = nums[i];
+                    j+=1;
+                }
+            }
+        return j;
+        }
+    }
+};
---
+++ Algorithm/Array/Remove Element.cpp
@@ -0,0 +1,24 @@
+/*
+Given an array and a value, remove all instances of that value in place and return the new length.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0;
+        int j=0;
+        if(nums.size()==0){
+            return 0;
+        }
+        else{
+            for(i=0;i<nums.size();i++){
+                if(nums[i]!=val){
+                    nums[j] = nums[i];
+                    j+=1;
+                }
+            }
+            return j;
+        }
+    }
+};
