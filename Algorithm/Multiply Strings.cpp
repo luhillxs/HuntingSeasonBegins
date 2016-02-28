dd817e1436ff9b12063f2f0b0bbe5400ede4b475
Hill Lo <hilllocanton@gmail.com>
2016-02-28 17:04:07 -0500

    optimize 1

--- Algorithm/Multiply Strings.cpp
+++ Algorithm/Multiply Strings.cpp
@@ -12,13 +12,13 @@ public:
             ptrr=num2.size()-1-ptr2;
             for(ptr1 = num1.size()-1;ptr1>=0;ptr1--){
                 string temp = to_string((num1[ptr1]-'0')*(num2[ptr2]-'0'));
-                temp.insert(0,"0");
-                reverse(temp.begin(),temp.end());
+                // temp.insert(0,"0");
+                // reverse(temp.begin(),temp.end());
                 int nextdig = 0;
                 int i;
                 for(i=0;i<temp.size();i++){
                     if(result[ptrr+i]<'0')  {result.replace(ptrr+i,1,"0");}
-                    int digit = temp[i] + result[ptrr+i] - 2*'0' + nextdig;
+                    int digit = temp[temp.size()-1-i] + result[ptrr+i] - 2*'0' + nextdig;
                     if(digit>9){
                         nextdig = digit/10;
                         digit = digit %10;
@@ -28,18 +28,16 @@ public:
                     }
                     result.replace(ptrr+i,1,to_string(digit));
                 }
-                if(result[ptrr+i]<'0')  {result.replace(ptrr+i,1,"0");}
-                result[ptrr+i] += nextdig;
+                if(nextdig!=0){
+                    if(result[ptrr+i]<'0')  {result.replace(ptrr+i,1,"0");}
+                    result[ptrr+i] += nextdig;
+                }
                 ptrr++;
             }
         }
-        int i;
-        for(i=result.size()-1;i>0;i--){
-            if(result[i]!='0') break;
-        }
-        result.erase(i+1);
+
         // if(result[result.size()-2]=='0') result.erase(result.size()-2);
-        // else if (result[result.size()-1]=='0') result.erase(result.size()-1);
+        // if (result[result.size()-1]=='0') result.erase(result.size()-1);
         reverse(result.begin(),result.end());
         return result;
     }
